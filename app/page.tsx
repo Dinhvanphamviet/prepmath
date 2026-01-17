fd833bc55be2528e70e448894bce1c5f15b23659
Dinh Van Pham Viet <vietphamdinhvan@gmail.com>
2026-01-17 23:04:01 +0700

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export const dynamic = "force-dynamic";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const session = await auth();
